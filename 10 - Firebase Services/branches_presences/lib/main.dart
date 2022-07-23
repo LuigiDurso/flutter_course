306acbf5075498030177e8c043817d71710ec6e3
Luigi Durso <alduin.93y@gmail.com>
2022-07-23 20:39:51 +0200

    Example 10: Added firebase integration

--- 10 - Firebase Services/branches_presences/lib/main.dart
+++ 10 - Firebase Services/branches_presences/lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'app/view/app.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   final storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory()
   );
